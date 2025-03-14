9ff2af5bee82d76dce1e486256a70a2e6569c0df
SlavikSliepchenko <viacheslavsliepchenko@gmail.com>
2025-03-14 19:34:45 +0100

    Change Homework4.java

--- Homework4.java
+++ Homework4.java
@@ -2,7 +2,7 @@ public class Homework4 {
 
     public static void main(String[] args) {
         System.out.println("This is my Homework. Practice");
-        System.out.println("This is branch2");
+        System.out.println("This is branch3");
 
     }
 }
