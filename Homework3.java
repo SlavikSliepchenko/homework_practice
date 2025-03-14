85e26e73664e6a4d8a16d367042dc900c4af6b2e
SlavikSliepchenko <viacheslavsliepchenko@gmail.com>
2025-03-14 19:01:11 +0100

    Change Homework3.java

--- Homework3.java
+++ Homework3.java
@@ -2,6 +2,7 @@ public class Homework3 {
 
     public static void main(String[] args) {
         System.out.println("This is my Homework. Practice");
+        System.out.println("This is branch2");
 
     }
 }
